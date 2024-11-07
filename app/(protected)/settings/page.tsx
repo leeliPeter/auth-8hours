4e97e75ddac2d1350a12398ea68e7476aa3a5eb1
Lei Ieong Tam <leeli.petertam@gmail.com>
2024-11-06 19:35:19 -0500

    auth callback

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,6 +1,7 @@
 import { auth, signOut } from "@/auth";
 export default async function SettingPage() {
   const session = await auth();
+  session?.user.role;
   return (
     <div>
       {JSON.stringify(session)}
