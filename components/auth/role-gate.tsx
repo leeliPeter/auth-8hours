87ece2d6b6be1e20354a4d62a0840870f66fae74
Lei Ieong Tam <leeli.petertam@gmail.com>
2024-11-17 19:07:37 -0500

    admin

---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,16 @@
+import { useCurrentRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
+import { FormError } from "../form-error";
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export default function RoleGate({ children, allowedRole }: RoleGateProps) {
+  const role = useCurrentRole();
+  if (role !== allowedRole) {
+    return <FormError message="You are not authorized to view this page" />;
+  }
+  return <>{children}</>;
+}
